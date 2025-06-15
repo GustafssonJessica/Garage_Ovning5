374eb593d3b916e4460c54fcde33cf5cf16f9c61
GustafssonJessica <jessica.gustafsson@live.se>
2025-06-15 19:56:03 +0200

    Början på vehicle

---
+++ Garage_Ovning5/Color.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_Ovning5
+{
+    internal enum Color
+    {
+        red, 
+        green, 
+        blue, 
+        yellow, 
+        grey, 
+        black, 
+        white, 
+        brown,
+        multicolor
+
+       
+    }
+}
--- Garage_Ovning5/Vehicles/Vehicle.cs
+++ Garage_Ovning5/Vehicles/Vehicle.cs
@@ -2,11 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Garage_Ovning5.Vehicles
 {
     internal class Vehicle
     {
+       
+        public string _registrationNumber { get; set; } //måste vara unikt
+        public Color Color {  get; set; }
+        public int NumberOfWheels { get; set; }
+
+
+
+        public string RegistrationNumber
+        {
+            get => _registrationNumber;
+            set
+            {
+                //om man gör det till en CharArray, och plats 0,1,2 är bokstav och plats 3,4,5 är siffror!
+                for (int i = 0; i < 3; i++)
+                {
+                    if (!Regex.IsMatch(value[i].ToString(), "^[a-öA-Ö]$"))
+                    {
+                        throw new ArgumentException("Ogiltigt registreringsnummer!");
+                    }
+                    if()
+                    
+                }
+                {
+
+                }
+            }
+        }
+
     }
 }
