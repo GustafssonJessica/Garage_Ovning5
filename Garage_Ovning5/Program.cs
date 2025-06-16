447c871d4fefa7ae8003fe4fbfb72e7cff009bcf
GustafssonJessica <jessica.gustafsson@live.se>
2025-06-16 13:31:04 +0200

    lagt till lite uppstartskod i program.cs

--- Garage_Ovning5/Program.cs
+++ Garage_Ovning5/Program.cs
@@ -1,33 +1,21 @@
-﻿namespace Garage_Ovning5
+﻿using System.Reflection.Metadata;
+
+namespace Garage_Ovning5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            UI ui = new UI(); // Du kan även använda dependency injection om du vill
+            GarageHandler garageHandler = new GarageHandler();
+
+            Manager manager = new Manager(ui, garageHandler);
+
+            manager.Run();
+
             //Skapar upp en manager (typ game.cs)
-         
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
 
 
 
